f2a0cce08b58851b74bf2a0c80aeafdfbd5a5505
Andrew Stuart <astuart@gonpl.com>
2013-07-19 16:55:40 -0700

    Added new functions.

--- routes/pictures.js
+++ routes/pictures.js
@@ -3,12 +3,24 @@
  * POST a picture.
  */
 
-//var fs = require('fs');
+var fs = require('fs');
 
 exports.pictures = function(req, res){
+    console.log(req.files);
 
-    res.sendfile(req.files.file.path);
-    /*res.render('picture', { title: 'YOU POSTED A PICTURE!'}, function(err, html) {
-        if(!err) res.sendfile(req.files.file.path);
-    });*/
+    fs.readFile(req.files.picture.path, function(err, data) {
+        var fileName = req.files.picture.name;
+        var newPath = "./public/" + fileName;
+
+        fs.writeFile(newPath, data, function(error) {
+            res.render('picture', { 
+                title: 'YOU POSTED A PICTURE!',
+                file: fileName,
+                fileTitle: fileName
+            })
+        });
+
+    });
+
+    //res.sendfile(req.files.picture.path);
 };
