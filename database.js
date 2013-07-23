71e53146127e61427f184f5dfe55aafc1220abd4
Andrew Stuart <astuart@gonpl.com>
2013-07-23 09:50:13 -0700

    ./database.js: Removed unused and unneeded code.

--- database.js
+++ database.js
@@ -9,36 +9,3 @@ Db.open(function(err) {
 });
 
 module.exports = Db;
-
-/*
-exports.run = function(app) {
-    db.open(function(err, db) {
-        if (err) throw err;
-
-        console.log("Connected to database!");
-
-        db.collection("foo", function(err, coll) {
-            debugger;
-            coll.find().toArray(function(err, docs) {
-
-                console.log(docs);
-            });
-
-            coll.find({}, function(err, results) {
-
-                results.toArray(function(err, data) {
-
-                    console.log(data)
-
-                })
-            })
-        })
-
-        app.files = new mongodb.Collection(db, 'files');
-
-        app.listen(app.get('port'), function(){
-            console.log('Express server listening on port ' + app.get('port'));
-        });
-    });
-}
-*/
