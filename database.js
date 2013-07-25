a8687347f0eac7536bdf044d613efea2f24b700b
Andrew Stuart <astuart@gonpl.com>
2013-07-24 17:52:59 -0700

    ./database.js: Added find function.

--- database.js
+++ database.js
@@ -8,4 +8,18 @@ Db.open(function(err) {
     console.log("Connected to database!");
 });
 
-module.exports = Db;
+module.exports.Db = Db;
+
+module.exports.find = function(collectionName, searchObject, callback) {
+  if(!collectionName) throw "Please at least pass a collection name."
+  searchObject = searchObject || {};
+  callback = typeof callback == 'function' ? callback : function() { return true };
+
+  Db.collection(collectionName, function(err, coll) {
+    coll.find(searchObject).toArray(function(err, returnArray) {
+      debugger;
+      callback(returnArray);
+    })
+  })
+}
+
