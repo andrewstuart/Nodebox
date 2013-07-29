cd5d2b113fb07d6f64fa17bf8f396dde14eef302
Andrew Stuart <astuart@gonpl.com>
2013-07-29 13:28:46 -0700

    ./routes/pictures.js: Getting ready to rip out unneeded data.

--- routes/pictures.js
+++ routes/pictures.js
@@ -20,30 +20,50 @@ var pad = function(n, width, z) {
 var intTest = /^\d+$/g;
 
 var parseFileRequest = function(req) {
-    //Get references to some variables.
-    var dbObject = _.extend({}, req.body);
-    var receivedTime = dbObject.receivedTime = new Date();
+  //Get references to some variables.
+  var dbObject = _.extend({}, req.body);
+  dbObject.fileIds = [];
+  var receivedTime = dbObject.receivedTime = new Date();
 
-    var jobNum = dbObject.jobPhaseNumber || 'None';
-    var areaNum = dbObject.areaNum = intTest.test(jobNum) ? jobNum.substring(0,2) : 'None';
+  var id = dbObject._id = dbObject._id ? ID(dbObject._id) : ID();
+  var jobNum = dbObject.jobPhaseNumber || 'None';
+  var areaNum = dbObject.areaNum = intTest.test(jobNum) ? jobNum.substring(0,2) : 'None';
 
-    var submittedAt = new Date(dbObject.submittedAt || '');
-    
-    //Get date folder from the submittedTime
-    var datepartString = submittedAt.getFullYear() + pad(submittedAt.getMonth(), 2);
+  var submittedAt = new Date(dbObject.submittedAt || '');
 
-    debugger;
+  //Get date folder from the submittedTime
+  var datepartString = submittedAt.getFullYear() + pad(submittedAt.getMonth(), 2);
 
-    var f = {file: req.files[fileName]};
-    f.folder = dbObject.fileFolder = defaultFolder + '/' + areaNum + '/' + jobNum + '/' + datepartString;
-    f.suffix = f.file.name.split('.').pop();
-    f.name = submittedAt.toJSON().replace(/:|\./g, '-') + jobNum + '.' + f.suffix;
-    f.path = dbObject.filePath = f.folder + '/' + f.name;
+  debugger;
 
-    return {
-      dbObject: dbObject,
-      f: f
-    }
+  var fileFolder = dbObject.fileFolder = defaultFolder + '/' + areaNum + '/' + jobNum + '/' + datepartString;
+  var fileArray = [];
+
+  for (fileKey in req.files) {
+    var eachFile = req.files[fileKey];
+    var suffix = eachFile.name.split('.').pop();
+    var fileObject = {
+      _id: ID(),
+      jobNum: jobNum,
+    };
+    fileObject.name = submittedAt.toJSON().replace(/:|\./g, '-') + jobNum + '.' + suffix;
+    fileObject.path = dbObject.filePath = fileFolder + '/' + fileObject.name;
+
+    fileArray.push(fileObject);
+    dbObject.fileIds.push( {
+      _id: fileObject._id,
+      path: fileObject.path
+    });
+
+    mkdirp(fileFolder, function(err) {
+      moveFile(eachFile.path, fileObject.path);
+    });
+  }
+
+  return {
+    dbObject: dbObject,
+      fileArray: fileArray
+  }
 }
 
 var moveFile = function(readPath, writePath) {
@@ -52,34 +72,22 @@ var moveFile = function(readPath, writePath) {
 
 var receivePost = function(req, res) {
   //Place to store data temporarily.
-  for (fileName in req.files) {
 
-    var reqDetails = parseFileRequest(req);
+  var reqDetails = parseFileRequest(req);
 
-    var f = reqDetails.f;
-    var dbObject = reqDetails.dbObject;
+  var dbObject = reqDetails.dbObject;
 
-    debugger;
+  debugger;
 
-    mkdirp(f.folder, function(err) {
-      moveFile(f.file.path, f.path);
+  //TODO: Refactor to use a data api.
+  //Insert the data into the file collection.
+  db.collection('files', function(err, fileCollection) {
+    if(err) throw err;
+    fileCollection.update({'_id': _id}, {$set: dbObject}, {upsert: true}, function(err, returnDocument) {
+      if(err) throw err;
+      exports.list(req, res);
     });
-
-    new Exif({image: f.file.path}, function(err, exifData) {
-
-      dbObject.exif = exifData;
-
-      //TODO: Refactor to use a data api.
-      //Insert the data into the file collection.
-      db.collection('files', function(err, fileCollection) {
-        if(err) throw err;
-        fileCollection.update({'filePath': f.path}, {$set: dbObject}, {upsert: true}, function(err, returnDocument) {
-          if(err) throw err;
-          exports.list(req, res);
-        });
-      });
-    });
-  }
+  });
 }
 
 exports.receive = receivePost;
